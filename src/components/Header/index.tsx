a9aafa92ca9bac45e615a4c4adefa4e0013ee38c
matheus <matheus@bolt360.com.br>
2024-10-21 13:52:56 -0300

    create app

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import styles from './styles.module.scss';
+
+export function Header() {
+    return (
+        <header className={styles.headerContainer}>
+            <div className={styles.headerContent}>
+                <img src="/ig.news.svg" alt="" />
+                <nav>
+                    <a href="" className={styles.active}>Home</a>
+                    <a href="" className={styles.active}>Posts</a>
+                </nav>
+            </div>
+        </header>
+    )
+}
